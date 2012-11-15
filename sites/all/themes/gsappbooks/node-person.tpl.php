1794232966804dccf0ad79128ff38df37b262105
Troy Conrad Therrien <troyth@gmail.com>
2012-11-15 14:07:22 -0500

    added books view to display for designers on /people/nid page

--- sites/all/themes/gsappbooks/node-person.tpl.php
+++ sites/all/themes/gsappbooks/node-person.tpl.php
@@ -11,8 +11,10 @@
     $viewName = 'book_by_author';
     $display_id_author = 'page_1';
     $display_id_editor = 'page_2';
+    $display_id_designer = 'page_3';
 
     print views_embed_view($viewName, $display_id_author, $node->nid);
     print views_embed_view($viewName, $display_id_editor, $node->nid);
+    print views_embed_view($viewName, $display_id_designer, $node->nid);
   ?>
 </div>
